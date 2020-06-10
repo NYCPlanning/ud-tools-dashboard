bc3588798e6c4565d05995957b0fe862aa54f1f3
Carsten Rodin <carsten.rodin@gmail.com>
2020-06-10 15:55:24 -0400

    cleanup on FAR feature

--- src/components/FAR.js
+++ src/components/FAR.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { formatNum } from '../utils/format'
+import React from 'react';
+import { formatFAR } from '../utils/format';
 
 export default function FAR({ pluginState }) {
-  const zoningLotArea = pluginState.SiteCurrent.ZoningLotArea
-  const zoning = pluginState.SiteCurrent.Scenarios[pluginState.ScenarioCurrent.Name]
-  const site = pluginState.SiteCurrent
+  const zoningLotArea = pluginState.SiteCurrent.ZoningLotArea;
+  const zoning = pluginState.SiteCurrent.Scenarios[pluginState.ScenarioCurrent.Name];
+  const site = pluginState.SiteCurrent;
+  const maxFAR = Math.max(zoning.ResidentialFAR, zoning.CommunityFacilityFAR, zoning.CommercialFAR, zoning.ManufacturingFAR);
 
   return (
     <div className='flex'>
@@ -16,27 +17,30 @@ export default function FAR({ pluginState }) {
                   <th className='w-1/6'>CF</th>
                   <th className='w-1/6'>C</th>
                   <th className='w-1/6'>M</th>
+                  <th className='w-1/6'></th>
                   <th className='w-1/6'>Total</th>
               </tr>
           </thead>
           <tbody>
               <tr className='text-left'>
-                  <th>{site.ZFA.Residential / zoningLotArea}</th>
-                  <th>{site.ZFA.CommunityFacility / zoningLotArea}</th>
-                  <th>{site.ZFA.Commercial / zoningLotArea}</th>
-                  <th>{site.ZFA.Manufacturing / zoningLotArea}</th>
+                  <th>{formatFAR(site.ZFA.Residential / zoningLotArea)}</th>
+                  <th>{formatFAR(site.ZFA.CommunityFacility / zoningLotArea)}</th>
+                  <th>{formatFAR(site.ZFA.Commercial / zoningLotArea)}</th>
+                  <th>{formatFAR(site.ZFA.Manufacturing / zoningLotArea)}</th>
                   <th>{0}</th>
+                  <th>{formatFAR(site.ZFA.Total / zoningLotArea)}</th>
               </tr>
               <tr className='text-left'>
-                  <th>{zoning.ResidentialFAR}</th>
-                  <th>{zoning.CommunityFacilityFAR}</th>
-                  <th>{zoning.CommercialFAR}</th>
-                  <th>{zoning.ManufacturingFAR}</th>
+                  <th>{formatFAR(zoning.ResidentialFAR)}</th>
+                  <th>{formatFAR(zoning.CommunityFacilityFAR)}</th>
+                  <th>{formatFAR(zoning.CommercialFAR)}</th>
+                  <th>{formatFAR(zoning.ManufacturingFAR)}</th>
                   <th>{0}</th>
+                  <th>{formatFAR(maxFAR)}</th>
               </tr>
           </tbody>
         </table>
       }
     </div>
   )
-}
+};
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,1 +1,2 @@
 export const formatNum = (n) => { return n.toLocaleString(undefined, {maximumFractionDigits:0}); };
+export const formatFAR = (n) => { return n.toFixed(2); };
