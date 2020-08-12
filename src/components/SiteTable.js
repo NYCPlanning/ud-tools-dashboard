80171e01c0e39b983cf5e083d51780944f3a12bc
Carsten Rodin <carsten.rodin@gmail.com>
2020-08-12 11:37:43 -0400

    visual FAR no longer proportional

--- src/components/SiteTable.js
+++ src/components/SiteTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { formatNum } from '../utils/format';
+import { formatNum, formatFAR } from '../utils/format';
 import * as color from '../utils/color';
 import VisualFAR from './VisualFAR';
 
@@ -30,31 +30,46 @@ export default function SiteTable({ pluginState }) {
                         <h3>R</h3>
                         <small>{formatNum(site.GFA.Residential)}</small>
                         <small>{formatNum(site.ZFA.Residential)}</small>
-                        <VisualFAR data={[utilized.R, allowed.R]} color={color.luYellow}/>
+                        <VisualFAR data={[utilized.R/allowed.R, 1]} color={color.luYellow}/>
+                        <span className={utilized.R > allowed.R ? 'text-red-500 font-bold' : ''}>
+                            <small>{formatFAR(utilized.R)} of {formatFAR(allowed.R)}</small>
+                        </span>        
                     </div>
                     <div className='flex flex-col justify-center items-center'>
                         <h3>CF</h3>
                         <small>{formatNum(site.GFA.CommunityFacility)}</small>
                         <small>{formatNum(site.ZFA.CommunityFacility)}</small>
-                        <VisualFAR data={[utilized.CF, allowed.CF]} color={color.luBlue}/>
+                        <VisualFAR data={[utilized.CF/allowed.CF, 1]} color={color.luBlue}/>
+                        <span className={utilized.CF > allowed.CF ? 'text-red-500 font-bold' : ''}>
+                            <small>{formatFAR(utilized.CF)} of {formatFAR(allowed.CF)}</small>
+                        </span>  
                     </div>
                     <div className='flex flex-col justify-center items-center'>
                         <h3>C</h3>
                         <small>{formatNum(site.GFA.Commercial)}</small>
                         <small>{formatNum(site.ZFA.Commercial)}</small>
-                        <VisualFAR data={[utilized.C, allowed.C]} color={color.luRed}/>
+                        <VisualFAR data={[utilized.C/allowed.C, 1]} color={color.luRed}/>
+                        <span className={utilized.C > allowed.C ? 'text-red-500 font-bold' : ''}>
+                            <small>{formatFAR(utilized.C)} of {formatFAR(allowed.C)}</small>
+                        </span>  
                     </div>
                     <div className='flex flex-col justify-center items-center'>
                         <h3>M</h3>
                         <small>{formatNum(site.GFA.Manufacturing)}</small>
                         <small>{formatNum(site.ZFA.Manufacturing)}</small>
-                        <VisualFAR data={[utilized.M, allowed.M]} color={color.luPurple}/>
+                        <VisualFAR data={[utilized.M/allowed.M, 1]} color={color.luPurple}/>
+                        <span className={utilized.M > allowed.M ? 'text-red-500 font-bold' : ''}>
+                            <small>{formatFAR(utilized.M)} of {formatFAR(allowed.M)}</small>
+                        </span>  
                     </div>
                     <div className='flex flex-col justify-center items-center'>
                         <h3>Total</h3>
                         <small>{formatNum(site.GFA.Total)} (GFA)</small>
                         <small>{formatNum(site.ZFA.Total)} (ZFA)</small>
-                        <VisualFAR data={[utilized.T, allowed.T]} color={'black'}/>
+                        <VisualFAR data={[utilized.T/allowed.T, 1]} color={'black'}/>
+                        <span className={utilized.T > allowed.T ? 'text-red-500 font-bold' : ''}>
+                            <small>{formatFAR(utilized.T)} of {formatFAR(allowed.T)}</small>
+                        </span>  
                     </div>
                 </div>
             }
