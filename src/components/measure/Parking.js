c333fd2e0889b3b38c452b9517a8066e9f60970f
Carsten Rodin <crodin@planning.nyc.gov>
2021-01-07 17:18:00 -0500

    shows assumptions in parking and unit counts

--- src/components/measure/Parking.js
+++ src/components/measure/Parking.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { formatNum } from '../../utils/format';
+import React, { useContext } from 'react'
+import { AssumptionsContext } from '../../App'
+import { formatNum } from '../../utils/format'
 
 export default function ParkingGeneric({ site, label }) {
+  const assumptions = useContext(AssumptionsContext)
   if ( !site ) return null;
   
   const key = label.replace(' ', '');
   const required = site[`${key}Required`];
-  const provided = site.GFA[key];
-  const thresholdMet = provided >= required.Area;
-  const areaString = `${formatNum(provided)} of ${formatNum(required.Area)} sqft`
+  const providedArea = site.GFA[key];
+  const providedSizeAssumption = assumptions[`${key.toUpperCase()}SPACEAREA`]
+  const providedSpaces = providedArea / providedSizeAssumption
+  const thresholdMet = providedArea >= required.Area;
+  const areaString = `${formatNum(providedArea)} of ${formatNum(required.Area)} sqft`
 
   return (
     <div className='flex flex-col mb-2'>
       <span className='font-bold'>{label}</span>
       <span className={thresholdMet ? '' : 'text-red-600'}>Area: {areaString}</span>
+      <span>Spaces: {formatNum(providedSpaces)}
+        <span className='assumption'> @ {providedSizeAssumption} sqft</span>    
+      </span>
       <span>Req. Spaces: {required.SpacesTotal}</span>
     </div>
   )
--- src/components/measure/UnitCounts.js
+++ src/components/measure/UnitCounts.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import { AssumptionsContext } from '../../App'
 
 export default function UnitCounts({ site }) {
-  if ( !site ) return null;
-
-  return (
+  const assumptions = useContext(AssumptionsContext)
+  if ( !site ) return null
+  else return (
     <div className='flex flex-col mb-2'>
       <span className='font-bold'>Units</span>
-      <span>Hotel Rooms: {site.Counts.HotelRooms}</span>
-      <span>Units: {site.Counts.ResidentialUnits}</span>
-      <span>Affordable Units @ 25%: {site.Counts.ResidentialUnitsAffordable25}</span>
+      <span>Hotel Rooms: {site.Counts.HotelRooms}
+        <span className='assumption'> @ {assumptions.HOTELROOMAREA} sqft</span>
+      </span>
+      <span>Units: {site.Counts.ResidentialUnits}
+        <span className='assumption'> @ {assumptions.RESIDENTIALUNITAREA} sqft</span>
+      </span>
+      {/* <span>Affordable Units @ 25%: {site.Counts.ResidentialUnitsAffordable25}</span> */}
     </div>
   )
-};
+}
