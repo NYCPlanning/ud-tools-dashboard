1176f274424e01a5e187e9ab1bd1a64556406a3c
Carsten Rodin <crodin@planning.nyc.gov>
2021-01-07 17:15:34 -0500

    rounds to integer in integer formatter

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
-export const formatNum = (n) => { return n.toLocaleString(undefined, {maximumFractionDigits:0}); };
+export const formatNum = (n) => { return Math.round(n).toLocaleString(undefined, {maximumFractionDigits:0}); };
 export const formatDim = (n) => { return n.toLocaleString(undefined, {maximumFractionDigits:2}); };
 export const formatFAR = (n) => { return n.toFixed(2); };
 export const currentISODate = () => format(new Date(), "yyyy-MM-dd")
